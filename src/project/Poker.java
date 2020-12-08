6bd34a4d79bf80f93a932afac926b4a3da0edeb4
JiaXin <batty890930@gmail.com>
2020-12-08 15:43:49 +0800

    pokercard class v1

---
+++ src/project/Poker.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		Pokercard card = new Pokercard(23);
+	}
+
+}
---
+++ src/project/Pokercard.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Pokercard {
+
+	int value;
+ 	int point;
+ 	char type;
+ 	public Pokercard(int value) {
+ 		this.value = value;
+ 	}
+	}
+
+
